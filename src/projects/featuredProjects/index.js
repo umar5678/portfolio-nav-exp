d1737a9015d389fa3351d87961647c5781f7de2c
Umar Farooq <97441126+umar5678@users.noreply.github.com>
2023-10-17 17:14:47 +0500

    Update index.js

--- src/projects/featuredProjects/index.js
+++ src/projects/featuredProjects/index.js
@@ -8,7 +8,7 @@ const featuredProjects = [
     id: 1,
     title: "Tesla Clone",
     description:
-      "This is a sample description of the project this is a tesla ui clone.This is a example project. In this project Lorem ipsum dolor sit amet, consectetur adipisicing.",
+      "Project Tesla UI Clone: A demonstration project showcasing a clone of the Tesla user interface. Developed using HTML, SCSS, JavaScript, and Firebase, and hosted on GitHub. Explore this example project to see a replica of Tesla's sleek user interface design.",
     url: "https://tesla-ui-clone-ten.vercel.app/",
     repo: "https://github.com/umar5678/Tesla-Clone",
     img: `${teslaImg}`,
@@ -18,7 +18,7 @@ const featuredProjects = [
     id: 2,
     title: "NikeShoe",
     description:
-      "This is a sample description of the project this is a tesla ui clone.This is a example project. In this project Lorem ipsum dolor sit amet, consectetur adipisicing.",
+      "NikeShoe Project: A web application project centered around Nike shoe products. Built with Tailwind CSS and React, it is deployed on Vercel for a seamless online experience. Discover this project to explore Nike's footwear collection in a dynamic and user-friendly interface.",
     url: "https://nike-shoe-rho.vercel.app/",
     repo: "https://github.com/umar5678/NikeShoe",
     img: `${nikeShoeImg}`,
@@ -29,7 +29,7 @@ const featuredProjects = [
     id: 3,
     title: "Coin App",
     description:
-      "This is a sample description of the Coin app project.This is a example project. In this project Lorem ipsum dolor sit amet, consectetur adipisicing.",
+      "Cion App: An application project, developed using React, SCSS, and JavaScript. The app is deployed on Vercel for easy access. Dive into this project to experience its interactive features and functionalities.",
     url: "https://react-crypto-app-seven-mu.vercel.app/",
     repo: "https://github.com/umar5678/coin-app",
     img: `${coinAppImg}`,
@@ -40,7 +40,7 @@ const featuredProjects = [
     id: 4,
     title: "React Cart",
     description:
-      "This is a sample description of the project.This is a example project. In this project Lorem ipsum dolor sit amet, consectetur adipisicing.",
+      "React Add to Cart Project: A web application showcasing an interactive 'Add to Cart' feature. Crafted with Styled Components and React, this project is hosted on GitHub for easy access. Explore and understand the shopping cart functionality with this example.",
     url: "https://react-cart-five-cyan.vercel.app/",
     repo: "https://github.com/umar5678/react-cart",
     img: `${reactCartImg}`,
